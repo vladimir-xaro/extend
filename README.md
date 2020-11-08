f1c657971c6cb14d40a7bfcdef8244de4413fee3
Vladimir Slavgorodskiy <vladimir.xaro@gmail.com>
2020-11-08 23:22:26 +0300

    release

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# extend
+# @xaro/extend
+
+Cut function from [swiper](https://github.com/nolimits4web/swiper)
+> [original source](https://github.com/nolimits4web/swiper/blob/master/src/utils/utils.js#L75)
+
+## License
+[MIT](LICENSE)
---
+++ index.js
@@ -0,0 +1,24 @@
+export default function extend(...args) {
+  const to = Object(args[0]);
+  for (let i = 1; i < args.length; i += 1) {
+    const nextSource = args[i];
+    if (nextSource !== undefined && nextSource !== null) {
+      const keysArray = Object.keys(Object(nextSource));
+      for (let nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex += 1) {
+        const nextKey = keysArray[nextIndex];
+        const desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+        if (desc !== undefined && desc.enumerable) {
+          if (isObject(to[nextKey]) && isObject(nextSource[nextKey])) {
+            extend(to[nextKey], nextSource[nextKey]);
+          } else if (!isObject(to[nextKey]) && isObject(nextSource[nextKey])) {
+            to[nextKey] = {};
+            extend(to[nextKey], nextSource[nextKey]);
+          } else {
+            to[nextKey] = nextSource[nextKey];
+          }
+        }
+      }
+    }
+  }
+  return to;
+}
