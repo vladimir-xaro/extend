3edc372e982bd2bd41be217c9bd16d5cd540887d
vladimir-xaro <vladimir.xaro@gmail.com>
2020-12-10 00:50:33 +0300

    refactoring code

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 export default function extend(...args) {
   const to = Object(args[0]);
-  for (let i = 1; i < args.length; i += 1) {
+
+  for (let i = 1; i < args.length; i++) {
     const nextSource = args[i];
-    if (nextSource !== undefined && nextSource !== null) {
-      const keysArray = Object.keys(Object(nextSource));
-      for (let nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex += 1) {
-        const nextKey = keysArray[nextIndex];
-        const desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
-        if (desc !== undefined && desc.enumerable) {
-          if (isObject(to[nextKey]) && isObject(nextSource[nextKey])) {
-            extend(to[nextKey], nextSource[nextKey]);
-          } else if (!isObject(to[nextKey]) && isObject(nextSource[nextKey])) {
-            to[nextKey] = {};
-            extend(to[nextKey], nextSource[nextKey]);
-          } else {
-            to[nextKey] = nextSource[nextKey];
-          }
-        }
+
+    if (nextSource === undefined || nextSource === null) {
+      continue;
+    }
+
+    const keysArray = Object.keys(Object(nextSource));
+    for (let nextIdx = 0, len = keysArray.length; nextIdx < len; nextIdx++) {
+      const nextKey = keysArray[nextIdx];
+      const desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+
+      if (desc === undefined || !desc.enumerable) {
+        continue;
+      }
+
+      if (isObject(to[nextKey]) && isObject(nextSource[nextKey])) {
+        extend(to[nextKey], nextSource[nextKey]);
+      } else if (!isObject(to[nextKey]) && isObject(nextSource[nextKey])) {
+        to[nextKey] = {};
+        extend(to[nextKey], nextSource[nextKey]);
+      } else {
+        to[nextKey] = nextSource[nextKey];
       }
     }
   }
+
   return to;
 }
 
-export function isObject(o) {
-  return typeof o === 'object' && o !== null && o.constructor && o.constructor === Object;
+export function isObject(obj) {
+  return typeof obj === 'object' && obj !== null && obj.constructor && obj.constructor === Object;
 }
